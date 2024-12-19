f0dbc5dcd8dfd9e98c665d4f9003c9b85b87f6d2
Rafael Miranda <rafa8626@gmail.com>
2024-12-19 16:12:03 -0500

    Changed IDs and region to retest Terraform pipeline

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-east-2"
 }
 
 resource "aws_s3_bucket" "rafa-test1" {
-  bucket        = "rafa-tf-bucket-1234567890"
+  bucket        = "rafa-tf-bucket-1234567898"
   force_destroy = true
 }
 
 terraform {
   backend "s3" {
-    bucket = "rafa-tf-bucket-1234567890"
+    bucket = "rafa-tf-bucket-1234567898"
     key    = "global/s3/terraform.tfstate"
-    region = "eu-west-2"
+    region = "us-east-1"
   }
 }
 
 resource "aws_instance" "test-instance1" {
-  ami           = "ami-0903ff18cc3c8e341"
+  ami           = "ami-0903ff18cc3c8e099"
   instance_type = "t2.micro"
   tags = {
     Name = "rafa-test-instance-${random_id.bucket_prefix.hex}"
